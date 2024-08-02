93086be90ba616a4eac37f453e3b929c3a942e9e
Matheus Braynner <matheus.souza@compasso.com.br>
2024-08-02 09:29:53 -0300

    feat: Created ms-catalog

---
+++ ms-catalog/src/main/java/io/github/matheusbraynner/ms_catalog/MsCatalogApplication.java
@@ -0,0 +1,13 @@
+package io.github.matheusbraynner.ms_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCatalogApplication.class, args);
+	}
+
+}
